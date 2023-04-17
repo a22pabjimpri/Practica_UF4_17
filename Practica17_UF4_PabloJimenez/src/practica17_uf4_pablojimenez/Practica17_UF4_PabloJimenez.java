1dca5ddab6a0e099a1cfc7cce09e5fa4f16e101f
ausias <ausias@hINFO6-5.xiam.cat>
2023-04-17 12:25:35 +0200

    diablo

---
+++ Practica17_UF4_PabloJimenez/src/practica17_uf4_pablojimenez/Practica17_UF4_PabloJimenez.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practica17_uf4_pablojimenez;
+
+/**
+ *
+ * @author ausias
+ */
+public class Practica17_UF4_PabloJimenez {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
